5d3954af6f5218c1742d06d735c5ec7f84533807
Leonardo Micheloni <leomicheloni@hotmail.com>
2019-12-12 12:04:52 +0100

    added slides link

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Ejemplos del workshop de Clean code
 
-
+Los slides de la charla están [acá](https://www.slideshare.net/leomicheloni/workshop-clean-code-javascript)
--- src/paint.test.js
+++ src/paint.test.js
@@ -12,13 +12,6 @@ describe('Paint', () => {
             paint.getPaintLeft("red", 2);
             paint.getPaintLeft("green", 3);
             const expected = '<table><thead><tr><td>Color</td><th>Remaining</th></tr></thead><tbody><tr><td>blue</td></tr>1</td></tr><tr><td>red</td></tr>2</td></tr><tr><td>green</td></tr>3</td></tr></tbody></table>';
-            // const expected =
-            //     '<table><thead>' +
-            //     '<tr><th>Color</th><th>Remaining</th></tr></thead><tbody>' +
-            //     '<tr><td>blue</td><td>7</td></tr>' +
-            //     '<tr><td>read</td><td>6</td></tr>' +
-            //     '<tr><td>green</td><td>5</td></tr>' +
-            //     '</tbody></table>'
 
             console.log(paint.generateReport());
             expect(paint.generateReport()).toEqual(expected);
